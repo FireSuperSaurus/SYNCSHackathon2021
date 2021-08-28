1e7481b26365ac1ac73ddc43ce9b5d50028b1ac5
FireSuperSaurus <42922954+FireSuperSaurus@users.noreply.github.com>
2021-08-29 03:59:50 +1000

    Polish pokemon card page

--- script.js
+++ script.js
@@ -14,3 +14,7 @@ function openEbay() {
 function openAmazon() {
     window.open('https://www.amazon.com.au/Marvel-Verse-Iron-Man-Comics-Marvel/dp/1302921177/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1630165978&sr=8-1');
 }
+
+function openEtsy() {
+    window.open('https://www.etsy.com/au/listing/1049495171/shadowless-charizard-holo-1st-ed-high?gpla=1&gao=1&&utm_source=google&utm_medium=cpc&utm_campaign=shopping_au_en_au_a-toys_and_games-games_and_puzzles-card_games-trading_card_games&utm_custom1=_k_CjwKCAjwmqKJBhAWEiwAMvGt6M5PMoX3MZuPOxq59TgmNGNCY8Pior3Vm2as_Ct9FslVG8XnqMvoGBoCjCgQAvD_BwE_k_&utm_content=go_344348048_45044724901_230399224441_pla-316175586843_c__1049495171enau_102845289&utm_custom2=344348048&gclid=CjwKCAjwmqKJBhAWEiwAMvGt6M5PMoX3MZuPOxq59TgmNGNCY8Pior3Vm2as_Ct9FslVG8XnqMvoGBoCjCgQAvD_BwE');
+}
