12cafa6639020c7d1c1a81939b582c6d5194c890
FireSuperSaurus <42922954+FireSuperSaurus@users.noreply.github.com>
2021-08-29 02:21:38 +1000

    polish iron man page

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 function openResult() {
     window.open('result.html');
 }
+
+function openBD() {
+    window.open('https://www.bookdepository.com/Marvel-verse-Iron-Man-Marvel-Comics/9781302921170?redirected=true&selectCurrency=AUD&w=AF45AU96LUMNXYA8V9FM&gclid=Cj0KCQjwvaeJBhCvARIsABgTDM6BS-LB-OlEBl_MHxYMEdVCpoSq2xUPJI8YW0dDR_w8TUvuyRGAMo0aAj-nEALw_wcB');
+}
+
+function openEbay() {
+    window.open('https://www.ebay.com.au/itm/304101513699?epid=8046400875&hash=item46cddcd9e3:g:d54AAOSwV-NhEqby');
+}
+
+function openAmazon() {
+    window.open('https://www.amazon.com.au/Marvel-Verse-Iron-Man-Comics-Marvel/dp/1302921177/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1630165978&sr=8-1');
+}
